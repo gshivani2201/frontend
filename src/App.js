26c50666101e2cf604679b6330721a8ce2f2018f
Shivani Gupta <shivani.gupta@paralleldots.com>
2023-06-25 17:12:29 +0530

    using variables

--- src/App.js
+++ src/App.js
@@ -60,12 +60,16 @@ class App extends Component {
     event.preventDefault();
     this.setState({ authLoading: true });
     const graphqlQuery = {
-      query: `{ 
-        login(email: "${authData.email}", password: "${authData.password}") {
+      query: `query UserLogin($email: String!, $password: String!) { 
+        login(email: $email, password: $password) {
           token
           userId
         }
-       }`
+       }`,
+       variables: {
+        email: authData.email,
+        password: authData.password
+       }
     }
     fetch('http://localhost:8080/graphql', {
       method: "POST",
@@ -117,17 +121,18 @@ class App extends Component {
     this.setState({ authLoading: true });
     const graphqlQuery = {
       query: `
-        mutation {
-          createUser(userInput: {email: "${
-            authData.signupForm.email.value
-          }", name:"${authData.signupForm.name.value}", password:"${
-        authData.signupForm.password.value
-      }"}) {
+        mutation CreateNewUser($email: String!, $password: String!, $name: String!) {
+          createUser(userInput: {email: $email, name: $name, password: $password}) {
             _id
             email
           }
         }
-      `
+      `,
+      variables: {
+        email: authData.signupForm.email.value,
+        name: authData.signupForm.name.value,
+        password: authData.signupForm.password.value
+      }
     };
     fetch("http://localhost:8080/graphql", {
       method: "POST",
