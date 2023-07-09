94ecd21a604fc84ef01c878e0b2a48b82cc0ca6f
Shivani Gupta <shivani.gupta@paralleldots.com>
2023-07-09 16:29:08 +0530

    removed slash

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 let BASE_URL =
   window.location.host === "localhost:3000"
     ? "localhost:8080"
-    : "postinger-o5u4.onrender.com/";
+    : "postinger-o5u4.onrender.com";
 
 export default BASE_URL;
